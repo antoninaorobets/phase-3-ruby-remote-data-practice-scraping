de95346640b0d8a5884f6598c2776b689213a482
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-22 12:56:09 -0400

    commit tree 0346156611c45f16e0d8d85ec881a540e5d7b62f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+doc.css(".heading-financier color-white mb-20 text-shadow animate animate-1s animated fadeInUp")
